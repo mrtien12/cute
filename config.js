fb1f2114a95f8d5a68dd757c6e1895fb0f98ceaf
tien_17 <taductien1712002@gmail.com>
2023-02-10 02:04:46 +0700

    first

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+	introTitle: "Nhật Anh à!",
+	introDesc: `Trái đất vốn lạ thường
+    Nhưng mà bạn dễ thương :))`,
+	btnIntro: "Ấn vào có điều bất ngờ hiii >_<",
+	title: "Nhật Anh dễ thương nhưng cũng dễ emotional nhiều lúc cũng rất đáng sợ ...😙",
+	desc: "...",
+	btnYes: "Đúng zậy <33",
+	btnNo: "Không, nói nhầm rồi ? :3",
+	question:
+		"Nhắn nhủ đôi điều đi voice càng tốt <3",
+	btnReply: "Gửi nào <3",
+	reply: "Dễ thương đúng không  <33333333",
+	mess: "Biết mà 🥰. Tuyệt vời 😘😘",
+	messDesc: "Gửi tặng ca khúc iii!",
+	btnAccept: "Okiiiii lun <3",
+	messLink: "https://www.messenger.com/t/100028828365299/",
+};
